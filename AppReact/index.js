bc617778fb97be8a99fc334825b9e06eb5ec5469
Diogo Vitorino <diogovf90@hotmail.com>
2022-06-30 11:41:15 -0300

    init react native -v 0.68

---
+++ AppReact/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import MainNavigator from './src/navigators/main';
+
+AppRegistry.registerComponent(appName, () => MainNavigator);
