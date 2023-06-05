b64eb57054594409b9058cc2d8b2ca7eca4aac16
Diogo Vitorino Franca <diogovf90@hotmail.com>
2023-06-05 11:01:28 -0300

    deleted default react-native project

--- AppReact/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import {name as appName} from './app.json';
-import MainNavigator from './src/navigators/main';
-
-AppRegistry.registerComponent(appName, () => MainNavigator);
